d8bfeb852f32b7baf1319b9e2269e81e0269d990
usama <usamaabubakar784@gmail.com>
2021-08-06 05:40:01 -0700

    heroku website hosting

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs")
+const app = express();
+const bodyParser = require('body-parser');
+const request = require("request");
+app.use(bodyParser.urlencoded({ extended: false }));
+const port=process.env.PORT || 8000;
+const temp_path = path.join(__dirname, "../template", "views");
+const static_path = path.join(__dirname, "../template/picsandcss");
+app.use(express.static(static_path));
+app.set("view engine", "hbs");
+app.set("views", temp_path);
+app.get("/",(req,res)=>{
+    res.render("search");
+})
+app.post("/",(req,res)=>{
+    const country=req.body.country;
+    request(`https://corona.lmao.ninja/v2/countries/${country}`,(err,resp)=>{
+        if(err){
+            res.render("404");
+        }
+        else{
+            
+            const data=JSON.parse(resp.body);
+            console.log(data.cases);
+            res.render("home",{
+                case:data.cases,
+                tcase:data.todayCases,
+                death:data.deaths,
+                tdeath:data.todayDeaths,
+                trecover:data.todayRecovered,
+                recover:data.recovered,
+                active:data.active,
+                critical:data.critical,
+                flag:data.countryInfo.flag,
+                con:country
+            })
+        }
+    })
+})
+app.listen(5000,()=>{
+    console.log("lisenteing....covid");
+})
